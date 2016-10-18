9422bdaeb1e813eccd1e2fe223a8da0281e5b75f
Ioanna <ioanna.kolovou@gmail.com>
2016-10-18 17:57:37 +0100

    small fixes

--- geometryFunctions/wktFunctions.py
+++ geometryFunctions/wktFunctions.py
@@ -7,6 +7,8 @@
 
 
 def vertices_from_wkt(wkt):
+    # the wkt representation may differ in other systems/ QGIS versions
+    # TODO: check
     nums = [i for x in wkt[11:-1:].split(',') for i in x.split(' ')]
     coords = zip(*[iter(nums)] * 2)
     for vertex in coords:
@@ -17,6 +19,7 @@ def vertices_from_wkt(wkt):
 
 
 def make_snapped_wkt(wkt, number_decimals):
+    # TODO: check in different system if '(' is included
     snapped_wkt = 'LINESTRING'
     for i in vertices_from_wkt(wkt):
         new_vertex = str(keep_decimals_string(i[0], number_decimals)) + ' ' + str(
