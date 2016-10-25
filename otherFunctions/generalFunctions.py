1932b88d951f8941d3404b99720b5c5a089ebfc8
joe <ioanna.kolovou@gmail.com>
2016-10-25 23:43:07 +0100

    addded angle_3_points calculation

--- otherFunctions/generalFunctions.py
+++ otherFunctions/generalFunctions.py
@@ -1,6 +1,7 @@
 
 # imports
 from itertools import tee, izip
+import math
 
 
 # make iterator from a list
@@ -53,8 +54,26 @@ def snap_coord(coord, tolerance):
 
 # find midpoint between two points
 
-
 def mid(pt1, pt2):
     x = (pt1.x() + pt2.x()) / 2
     y = (pt1.y() + pt2.y()) / 2
     return (x, y)
+
+
+def angle_3_points(inter_point, vertex1, vertex2):
+    inter_vertex1 = math.hypot(abs(inter_point.asPoint()[0] - vertex1[0]),
+                               abs(inter_point.asPoint()[1] - vertex1[1]))
+    inter_vertex2 = math.hypot(abs(inter_point.asPoint()[0] - vertex2[0]),
+                               abs(inter_point.asPoint()[1] - vertex2[1]))
+    vertex1_2 = math.hypot(abs(vertex1[0] - vertex2[0]), abs(vertex1[1] - vertex2[1]))
+    A = ((inter_vertex1 ** 2) + (inter_vertex2 ** 2) - (vertex1_2 ** 2))
+    B = (2 * inter_vertex1 * inter_vertex2)
+    if B != 0:
+        cos_angle = A / B
+    else:
+        cos_angle = NULL
+    if cos_angle < -1:
+        cos_angle = int(-1)
+    if cos_angle > 1:
+        cos_angle = int(1)
+    return math.acos(cos_angle)
