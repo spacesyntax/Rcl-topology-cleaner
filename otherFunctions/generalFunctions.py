68262a2d9ba4c9997138fb396b70d3f999d387d6
Ioanna <ioanna.kolovou@gmail.com>
2016-10-28 17:26:41 +0100

    changed angle 3 points in degrees

--- otherFunctions/generalFunctions.py
+++ otherFunctions/generalFunctions.py
@@ -76,4 +76,4 @@ def angle_3_points(inter_point, vertex1, vertex2):
         cos_angle = int(-1)
     if cos_angle > 1:
         cos_angle = int(1)
-    return math.acos(cos_angle)
+    return math.degrees(math.acos(cos_angle))
